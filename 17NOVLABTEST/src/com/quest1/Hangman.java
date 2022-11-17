05d865b4f8d64c448c280c03d6c803e9ee5655f7
hp <hp@LAPTOP-HB0RKING>
2022-11-17 12:59:34 +0530

    LAB TEST 17 November

---
+++ 17NOVLABTEST/src/com/quest1/Hangman.java
@@ -0,0 +1,36 @@
+package com.quest1;
+
+import java.util.Scanner;
+
+public class Hangman {
+	
+	public static void main(String args[])
+	{
+		String arr[] = new String[]{"PRANJAL","SAINA", "SAURAV", "SUMAN", "SWATI"};
+		System.out.println("-----------WELCOME TO HANGMAN GAME----------");
+		System.out.println("Select an array Index to choose words: ");
+		System.out.println("Array Index should not be greater than 5");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int len = arr[n].length();
+		System.out.println("Input your guessing: ");
+		for(int i=0; i<len; i++)
+		{
+			System.out.print("_ ");
+		}
+		System.out.println();
+		
+		String a = sc.next().toUpperCase();
+		if(a == arr[n])
+		{
+			System.out.println("Your Guess was correct");
+		}
+		else
+		{
+			System.out.println("You loose");
+		}
+		sc.close();
+		
+	}
+
+}
---
+++ 17NOVLABTEST/src/com/quest4/Calculator.java
@@ -0,0 +1,41 @@
+package com.quest4;
+
+public class Calculator {
+	
+
+	
+	
+		//method for addition
+		public static float addition(float num1, float num2)
+		{
+			return num1+num2;
+		}
+		
+		//method for substraction
+		public static float substraction(float num1, float num2)
+		{
+			return num1-num2;
+		}
+		
+		//method for multiplication
+		public static float multiplication(float num1, float num2)
+		{
+			return num1*num2;
+		}
+		
+		//method for division
+		public static float division(float num1, float num2)
+		{
+			float c =num1/num2;
+			try {float c;}
+			catch(ArithmeticException e){
+				System.out.println(e.getMessage());
+				
+			}
+			return c;
+			
+		}
+	
+
+
+}
