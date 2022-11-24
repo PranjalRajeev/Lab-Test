8f2c47c1f4c2cc1dc81f7c1dedacff5091ae2d38
hp <hp@LAPTOP-HB0RKING>
2022-11-24 12:56:25 +0530

    24 Nov LAB TEST

---
+++ 24NOVLABTEST/src/com/quest2/Inheritence.java
@@ -0,0 +1,20 @@
+package com.quest2;
+
+public class Inheritence {
+	
+	public void display()
+	{
+		System.out.println("This is a Parent Class");
+	}
+
+}
+
+class B extends Inheritence{
+
+	public void display()
+	{
+		super.display();
+		System.out.println("This is a Child Class");
+	}
+}
+
